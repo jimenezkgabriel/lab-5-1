54b2f3e8c18e1344a6b6892ff852b2ed2b3de52b
jimenezkgabriel <jimenez.k.gabriel@gmail.com>
2025-11-12 01:44:11 -0500

    added everything else

---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+1. How did you dynamically create and append new elements to the DOM?
+    * I created a template HTML that had all the elements I wanted to create and append, then I cloned it to make a new row with the new values entered from the user.
+2. What steps did you take to ensure accurate updates to the total price?
+ * I fired off an event that updates the total price whenever an action was performed
+3. How did you handle invalid input for product name or price?
+ * I turned the string into a Number() object, then used a Number method called Number.isNan() which checks if the input is valid or not
+4. What challenges did you face when implementing the remove functionality?
+ * I had to figure out how to remove the row of the called element in question, which my button was nested within a few divs. I used a function .closest() then checked for the closest tr element, which finds its own parent tr element then I call .remove() on that tr.
---
+++ scripts/script.js
@@ -0,0 +1,61 @@
+const inputProduct = document.getElementById(`productName`);
+const inputPrice = document.getElementById(`productPrice`)
+const btnAddProduct = document.getElementById(`addProduct`);
+const tableCart = document.getElementById(`cart`);
+const totalPrice = document.getElementById(`totalPrice`);
+
+const templateProduct = document.getElementById(`templateProduct`);
+const templatePrice = document.getElementById(`templatePrice`);
+const templateQuantity = document.getElementById(`templateQuantity`);
+const templateRemove = document.getElementById(`templateRemove`);
+
+btnAddProduct.addEventListener(`click`, (e) => {
+    templateProduct.textContent = inputProduct.value;
+    templatePrice.value = Number.isNaN(Number(inputPrice.value)) ? 0.00 : Number(inputPrice.value).toFixed(2);
+    templateQuantity.value = 1;
+    const clonedNode = document.getElementById(`template`).cloneNode(true);
+    templateProduct.textContent = ``;
+    templatePrice.value = ``;
+    templateQuantity.value = ``;
+    clonedNode.style.display = ``;
+    tableCart.appendChild(clonedNode);
+    console.log(clonedNode);
+    inputProduct.value = ``;
+    inputPrice.value = ``;
+    calculateTotal();
+})
+
+tableCart.addEventListener(`click`, (e) => {
+    if (e.target.classList.contains(`remove`)) {
+        e.target.closest(`td`).parentElement.remove();
+    }
+
+    if (e.target.classList.contains(`price`)) {
+        e.target.addEventListener(`blur`, (e) => {
+            if (!Number.isNaN(Number(e.target.value))) {
+                calculateTotal();
+            }
+        })
+    }
+    if (e.target.classList.contains(`quantity`)) {
+        e.target.addEventListener(`blur`, (e) => {
+            if (!Number.isNaN(Number(e.target.value))) {
+                e.target.parentElement.previousElementSibling.lastChild.value *= e.target.value;
+                calculateTotal();
+            }
+        })
+    }
+})
+
+let calculateTotal = () => {
+    let priceList = document.getElementsByClassName(`price`);
+    let sum = 0;
+    for (let p of priceList) {
+        sum += Number(p.value);
+        totalPrice.textContent = ``;
+        totalPrice.textContent = `Total Price: ${new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }).format(sum)}`;
+    }
+}
